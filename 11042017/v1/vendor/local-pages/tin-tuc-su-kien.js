12a5bb805061370558bd88d6ee3434ae4bad7cc3
hungdlv <hungdlv@gmail.com>
2017-04-20 17:26:16 +0700

    Update

---
+++ 11042017/v1/vendor/local-pages/tin-tuc-su-kien.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#video1').videoExtend({
+        logo: '11042017/v1/assets/logo-150x29.png',
+        logoLink: 'http://giadinhquan.com/',
+        logoSize: [ 150, 29 ],
+        logoPosition: [ 'auto', 10, 50, 'auto' ] // top, right, bottom, left
+    });
+    $('#video2').videoExtend({
+        logo: '11042017/v1/assets/logo-150x29.png',
+        logoLink: 'http://giadinhquan.com/',
+        logoSize: [ 150, 29 ],
+        logoPosition: [ 'auto', 10, 50, 'auto' ] // top, right, bottom, left
+    });
+});
