044e14c10c8e93d17d24dbab4b63f847b9fcb7cb
hungdlv <hungdlv@gmail.com>
2017-04-21 23:05:11 +0700

    Update

--- 11042017/v1/vendor/local-pages/tin-tuc-su-kien.js
+++ 11042017/v1/vendor/local-pages/tin-tuc-su-kien.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
+    
+var Screen = function () { return { width: $(window).width(), height: $(window).height() }; } 
+$(document).ready(function(){       
+    var videos = document.getElementsByTagName("video");
+    for(var i=0; i< videos.length; i++){
+        videos[i].setAttribute('height', 90*Screen().height/100);
+        videos[i].setAttribute('width', 90*Screen().width/100);
+    }
+
+
     $('#video1').videoExtend({
         logo: '11042017/v1/assets/logo-150x29.png',
         logoLink: 'http://giadinhquan.com/',
@@ -11,4 +20,8 @@ $(document).ready(function(){
         logoSize: [ 150, 29 ],
         logoPosition: [ 'auto', 10, 50, 'auto' ] // top, right, bottom, left
     });
+
+    
+
+    
 });
